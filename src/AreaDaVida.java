e3707d9c27b8d9e774092d3e4485f608c57a1e58
marialuisa214 <211039617@aluno.unb.br>
2022-08-10 11:56:34 -0300

    Inicializando projeto - primeiras classes

---
+++ src/AreaDaVida.java
@@ -0,0 +1,20 @@
+package projetoHabitos;
+
+public class AreaDaVida {
+//ATRIBUTOS
+	private String nome;
+	//private Habito habitoRelacionado;
+	
+//CONSTRUTOR
+	public AreaDaVida(String nome) {
+		this.nome = nome;		
+	}
+
+//METODOS
+	public String getNome() {
+		return nome;}
+	public void setNome(String nome) {
+		this.nome = nome;}
+	
+	//CRIARNOVOHABITO
+}
---
+++ src/Habito.java
@@ -0,0 +1,39 @@
+package projetoHabitos;
+
+public abstract class Habito {
+	
+//ATRIBUTOS
+	protected String nome;
+	//protect Lembrete aviso;
+	protected int repeticaoDia;
+	protected String turno; //usar enum posteriomente
+	protected int feito;
+	protected boolean status;
+
+//METODOS
+	public String getNome() {
+		return nome;}
+	public void setNome(String nome) {
+		this.nome = nome;}
+	
+	public int getRepeticaoDia() {
+		return repeticaoDia;}
+	public void setrepeticaoDia(int repeticoes) {
+		this.repeticaoDia = repeticoes;}
+	
+	public String getTurno() {
+		return turno;}
+	public void setTurno(String turno) {
+		this.turno = turno;}
+	
+	public int getFeito() {
+		return feito;}
+	public void setFeito(int feito) {
+		this.feito = feito;}
+	
+	public boolean getStatus() {
+		return status;}
+	public void setStatus(boolean status) {
+		this.status = status;}
+
+}
